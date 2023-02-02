37c77934dcc17ed7bb87fd2da08d733cd9d7ea10
tkl0000 <thomasli2025@gmail.com>
2023-02-02 07:31:14 -0500

    new main

--- ascii.py
+++ ascii.py
@@ -3,12 +3,14 @@ import os
 import re
 
 def main():
+    renderImage('cat.jpg', 256)
+
+def renderImage(file, maxSize):
     #get current directory
     dir = os.path.dirname(os.path.abspath(__file__))
 
     #open target file & convert to grayscale
-    file = input("Enter image name: ")
-    imagePath = dir + '/' + file + '.jpg'
+    imagePath = dir + '/' + file
     image = Image.open(imagePath, 'r')
     imageName = (re.split("/|.jpg", imagePath))
     imageName = (imageName[len(imageName)-2])
@@ -17,28 +19,26 @@ def main():
     ### different gradients to choose from
     # characters = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'."""
     # characters = """@@@@@@@@@@@@%%%%%%%%#########********+++++++++===="""
-    characters = """@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:'-.`"""
+    # characters = """@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:'-.`"""
+    characters = """`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"""
     scale = [*characters]
 
 
-    #get image size and ratio - calculate new dimensions - smallest dimension must be 16
+    #get image size
     size = image.size
     w = size[0]
     l = size[1]
 
-    #play w/ different values
-    maxSize = int(input('Enter max size (px): '))
-
     newSizes = findNewDimensions(w, l, maxSize)
     image = image.convert('L')
     image = image.resize(newSizes)
-    image.save(dir + '/new_' + imageName + '.jpg')
+    # image.save(dir + '/new_' + imageName + '.jpg')
 
     toPixel = []
     totalMax = image.getpixel((0, 0))
     totalMin = image.getpixel((0, 0))
 
-    print(newSizes)
+    # print(newSizes)
 
     for y in range(0, newSizes[1], 2):
         row = []
