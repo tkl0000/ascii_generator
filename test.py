bebebaaa07db7193f091b52b91faecfc55557044
tkl0000 <thomasli2025@gmail.com>
2023-11-03 14:05:19 -0400

    changes

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-str = list("""`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@""")
-print(''.join(str[::-1]))
-dir = os.path.dirname(os.path.abspath(__file__))
-os.mkdir(dir + '/frames')
